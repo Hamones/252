fdfd88880c331b84134130fd711c4a978d4abfcc
Hamones <63001546+Hamones@users.noreply.github.com>
2025-09-05 19:39:11 -0300

    Create selectsort.c

---
+++ selectsort.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void selection_sort(int n, int vet[])
+{
+  if (n==1)
+  {
+    return v[n];
+    else
+    (v[n] < selection_sort(n-1;vet)
+    troca (v[n],vet);
+  }
+}
+  int main()
+{
+  selection_sort (n,vet);
+
+
+  pegar a próxima função
+
+
+  retunrn 0;
+}
