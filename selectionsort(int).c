af871927098f7c7fa20499b4859e8279dda84637
Hamones <63001546+Hamones@users.noreply.github.com>
2025-09-05 20:06:04 -0300

    Create selectionsort(int).c

---
+++ selectionsort(int).c
@@ -0,0 +1,15 @@
+void selectionsort(int n, int vet[])
+{
+  int i, aux;
+  int vet[];
+  
+  for (int i= 0;i < (n-1);i++)
+  {
+    if ( v[i] < v[j])
+    {
+      aux = v[i];
+      v[i] = v[j];
+      v[j] = aux;
+    }
+  }
+}
